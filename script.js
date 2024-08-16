8ec66122cb17d46b9b3c166bc518e6c1f6bf38c7
leonardo vargas <Leonardo.vargas1971br@gmail.com>
2024-08-16 11:10:27 -0300

    teste
    gg

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("aviso!")
+console.log(" teste de console")
+document.getElementById("demo").innerHTML = "sandro Martins";
+let nome = prompt(" digite seu nome")
+alert(" Ola "+nome+", tudo bem?")
